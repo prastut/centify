0b0cbac6219a32a300974087c54642a465e14cec
aviral srivastava <aviral.srivastava@wingify.com>
2018-08-11 15:52:03 +0530

    removes technical debts.

--- trenity/server/helper.js
+++ trenity/server/helper.js
@@ -3,13 +3,6 @@ const moment = require("moment");
 const fs = require("fs-extra");
 const util = require("util");
 
-//Technical Debt
-const { ABSOLUTE_MATCH_START_TIME, SIM_MATCH_FILE } = require("./variables");
-const readFile = util.promisify(fs.readFile);
-
-const maxEmotion = emotions =>
-  Object.keys(emotions).reduce((a, b) => (emotions[a] > emotions[b] ? a : b));
-
 const getTimeInsideMatch = (simulatedMatchStartTime, collection) => {
   const now = moment.utc();
   const absoluteMatchStartTime = getAbsoluteMatchStartTime(collection);
@@ -17,21 +10,6 @@ const getTimeInsideMatch = (simulatedMatchStartTime, collection) => {
   return moment.utc(absoluteMatchStartTime + period);
 };
 
-const writeSimulationTimeToFile = async () => {
-  await fs.writeFile(SIM_MATCH_FILE, moment.utc().format());
-};
-
-const readSimulationTimeFromFile = async () =>
-  moment.utc(await readFile(SIM_MATCH_FILE, "utf-8"));
-
-const getAbsoluteMatchStartTime = collection => {
-  console.log(ABSOLUTE_MATCH_START_TIME[collection]);
-  return ABSOLUTE_MATCH_START_TIME[collection];
-};
-
 module.exports = {
-  maxEmotion,
-  getTimeInsideMatch,
-  writeSimulationTimeToFile,
-  readSimulationTimeFromFile
+  getTimeInsideMatch
 };
