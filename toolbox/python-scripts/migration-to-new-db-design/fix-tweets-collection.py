3e872c6254472697504cd3198683e7b1429138c9
prastut <kr.prastut@gmail.com>
2018-08-15 17:14:16 +0530

    WIP

--- toolbox/python-scripts/migration-to-new-db-design/fix-tweets-collection.py
+++ toolbox/python-scripts/migration-to-new-db-design/fix-tweets-collection.py
@@ -1,6 +1,14 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient("mongodb://bubble:bubble@104.196.215.99:27017/Bubble")
+MONGO_URL = os.getenv(
+    'MONGODB_URI', "mongodb://bubble:bubble@104.196.215.99:27017/Bubble")
+
+print '-----------------------------------------'
+print MONGO_URL
+print ""
+
+client = MongoClient(MONGO_URL)
 db_pre_epl = client["Bubble"]
 db = client["EPL"]
 
@@ -9,7 +17,7 @@ collection_id = "5b6efa5989d81053b5621f66"
 
 
 print ('-----------------------------------------')
-print "Migrating Tweets from Bubble to EPL for" + collection_pre_epl_id
+print "Migrating Tweets from Bubble to EPL for " + collection_pre_epl_id
 print ""
 
 for post in db_pre_epl[collection_pre_epl_id].find({}, {"_id": False}):
