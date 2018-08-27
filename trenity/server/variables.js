db294e4047f90f9415009ea5548176f926296bf0
prastut <kr.prastut@gmail.com>
2018-08-27 23:43:40 +0530

    WIP

--- trenity/server/variables.js
+++ trenity/server/variables.js
@@ -5,9 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 const BUILD_DIRECTORY = path.join(__dirname, "../client/build");
 
-const DB_URL =
-  process.env.MONGODB_URI ||
-  "mongodb://bubble:bubble@104.196.215.99:27017/Bubble";
+const DB_URL = process.env.MONGODB_URI;
 
 const DATABASE = "EPL";
 
